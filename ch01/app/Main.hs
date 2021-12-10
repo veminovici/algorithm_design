5a6b3769b50300efbba817fe2932fc94ea86aca7
veminovici <veminovici@hotmail.com>
2021-12-10 23:11:15 +0200

    Add exercises for ch01

--- ch01/app/Main.hs
+++ ch01/app/Main.hs
@@ -2,5 +2,51 @@ module Main where
 
 import Lib
 
+--
+-- INDUCTIVE
+--
+
+-- The idiom foldr (concatMap · steps) e will be used frequently
+
+perms1 :: [a] -> [[a]]
+perms1 [] = [[]]
+perms1 (x:xs) = [ zs | ys <- perms1 xs, zs <- inserts x ys]
+
+inserts :: t -> [t] -> [[t]]
+inserts x [] = [[x]]
+inserts x (y:ys) = (x:y:ys) : map (y:) (inserts x ys)
+
+perms2 :: [a] -> [[a]]
+perms2 = foldr step [[]]
+    where
+        step x xss = concatMap (inserts x) xss
+
+--
+-- RECURSIVE
+--
+
+perms3 :: [a] -> [[a]]
+perms3 [] =[[]]
+perms3 xs = [x:zs| (x,ys) <- picks xs, zs <- perms3 ys]
+
+-- picks an arbitrary element from a list in all possible ways, returning both the element and what remains
+picks :: [a] -> [(a, [a])]
+picks [] = []
+picks (x:xs) = (x, xs) : [(y, x:ys)| (y, ys) <- picks xs]
+
+perms4 :: [a] -> [[a]]
+perms4 [] = [[]]
+perms4 xs = concatMap subperms (picks xs)
+    where
+            subperms (x, ys) = map (x:) (perms4 ys)
+
 main :: IO ()
-main = someFunc
+main = do
+    let xs = inserts 1 [2, 3]
+    print $ "xs=" ++ show xs
+
+    let xs = perms1 [1, 2, 3]
+    print $ "xs=" ++ show xs
+
+    let xs = perms2 [1, 2, 3]
+    print $ "xs=" ++ show xs
--- ch01/src/Lib.hs
+++ ch01/src/Lib.hs
@@ -1,6 +1,56 @@
 module Lib
     ( someFunc
     ) where
+import GHC.Float (int2Double)
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+vxMaximum :: Ord a => [a] -> Maybe a
+vxMaximum [] = Nothing 
+vxMaximum [x] = Just x
+vxMaximum (x:xs) = (max <$> Just x) <*> vxMaximum xs
+
+vxTake :: Int -> [a] -> [a]
+vxTake 0 xs = []
+vxTake n [] = []
+vxTake n (x:xs) = x : vxTake (n - 1) xs
+
+vxTakeWhile :: (a -> Bool) -> [a] -> [a]
+vxTakeWhile p [] = []
+vxTakeWhile p (x:xs) | p x = x : vxTakeWhile p xs
+vxTakeWhile p (_:xs) = []
+
+vxDrop :: Int -> [a] -> [a]
+vxDrop 0 xs = xs
+vxDrop n [] = []
+vxDrop n (x:xs) = vxDrop (n - 1) xs
+
+vxUncons :: [a] -> Maybe (a, [a])
+vxUncons [] = Nothing
+vxUncons (x:xs) = Just (x, xs)
+
+vxWrap :: a -> [a]
+vxWrap x = [x]
+
+single :: [a] -> Bool
+single [x] = True 
+single _ = False
+
+vxReverse :: [a] -> [a]
+vxReverse = foldl (flip (:)) []
+
+vxMap :: (a -> b) -> [a] -> [b]
+vxMap f = foldr op [] where op x xs = f x : xs
+
+vxFilter :: (a -> Bool) -> [a] -> [a]
+vxFilter f = foldr op [] where op x xs = [x | f x] ++ xs
+
+vxDropWhileEnd :: (a -> Bool) -> [a] -> [a]
+vxDropWhileEnd f xs = snd $ foldr (\x (p, acc) -> if p && f x then (p, acc) else (False, x : acc)) (True, []) xs
+
+vxInteger :: [Int] -> Int
+vxInteger = foldl (\acc x -> 10 * acc + x) 0
+
+vxFraction :: [Int] -> Double
+vxFraction = foldr (\x acc -> int2Double x + acc * 0.1) 0.0
