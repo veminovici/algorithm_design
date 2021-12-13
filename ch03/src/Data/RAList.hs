23442538dec99b96fb697466c0681645b4bed772
veminovici <veminovici@hotmail.com>
2021-12-13 10:49:53 +0200

    Add the stubs for the public RA functions

--- ch03/src/Data/RAList.hs
+++ ch03/src/Data/RAList.hs
@@ -34,8 +34,44 @@ fromTree :: Tree a -> [a]
 fromTree (Leaf x) = [x]
 fromTree (Node _ t1 t2) = fromTree t1 ++ fromTree t2
 
+-- | Returns the collection of items
 fromRAList :: RAList a -> [a]
 fromRAList = concatMap from 
     where 
     from Zero = []
     from (One t) = fromTree t
+
+-- | Returns true if the list is empty
+nullRA :: RAList a -> Bool
+nullRA [] = True
+nullRA [Zero] = True
+nullRA _ = False
+
+-- | Returns the empty list
+nilRA = []
+
+-- | Adds a new element at the begining of the list
+consRA :: a -> RAList a -> RAList a
+consRA = undefined
+
+-- | Splits the list in a pair of header and tail
+unconsRA :: RAList a -> (a, RAList a)
+unconsRA = undefined 
+
+-- | Updates the item at the given position
+updateRA :: Int -> a -> RAList a -> RAList a
+updateRA = undefined
+
+fetchTree :: Int -> Tree a -> a
+fetchTree 0 (Leaf x) = x
+fetchTree k (Node n t1 t2) = 
+    if k < m
+    then fetchTree k t1 else fetchTree (k - m) t2
+    where
+    m = n `div` 2
+fetchTree _ _ = error "fetchTree - unexpected"
+
+fetchRA :: Int -> RAList a -> a
+fetchRA k (Zero:xs) = fetchRA k xs
+fetchRA k (One t:xs) = let s = size t in if k < s then fetchTree k t else fetchRA (k - s) xs
+fetchRA _ _ = error "fetchRA - unexpected"
